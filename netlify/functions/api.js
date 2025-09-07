8131ce8af1334c3b122aff4550d5b02181f7be60
makrus636 <aliamem16@gmail.com>
2025-09-07 22:23:23 +0700

    2 commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,6 +4,7 @@ const serverless = require('serverless-http');
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
+        host: 'localhost',
     });
 
     server.route({
