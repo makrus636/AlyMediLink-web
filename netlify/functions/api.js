f74b9e5376a4fd7e4aff824892faf79626af3bd0
makrus636 <aliamem16@gmail.com>
2025-09-07 22:14:40 +0700

    2 commit

---
+++ netlify/functions/api.js
@@ -0,0 +1,26 @@
+const Hapi = require('@hapi/hapi');
+const serverless = require('serverless-http');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => ({ message: 'Hello, World!' }),
+    });
+    await server.initialize();
+    return server.listener;
+
+}
+
+let handler;
+module.exports.handler = async (event, context) => {
+    if (!handler) {
+        const server = await init();
+        handler = serverless(server);
+    }
+    return handler(event, context);
+}
