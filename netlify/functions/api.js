b36190783244945f677eef36b2414d66d61d71c3
makrus636 <aliamem16@gmail.com>
2025-09-07 22:33:49 +0700

    2 commit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -9,7 +9,7 @@ const init = async () => {
 
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/.netlify/functions/api',
         handler: () => { return { message: 'Hello, world!' }; },
     });
     await server.initialize();
